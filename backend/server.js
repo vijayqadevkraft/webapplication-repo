03a7f8a9f2e21a3731d91556e024507b28e417af
Vijay Kumar <95744146+vijayqadevkraft@users.noreply.github.com>
2026-04-02 17:24:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer((req, res) => {
 
-    if (req.url === '/api') {
+    if (req.url.startsWith('/api')) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end("Hello from Backend 🚀");
     } else {
